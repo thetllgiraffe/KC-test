24c24218e53f1a455a23f47d6de767f79256c5e1
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-06-16 23:26:06 -0700

    refactor(Login): add bg image, apply consistent h2 style for name, anchor overlay

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,13 @@
 import "../style.css";
 import "./login.css";
+import robo from "/neural_robot.png";
 
 export function renderLogin() {
   document.querySelector("#app").innerHTML = `
   <div class="login-container">
+    <img src="${robo}" alt="neural robot" class="robot-bg" />
     <div class="login-card">
-      <h2>Welcome Back</h2>
+      <h2>Kard Chips</h2>
       <form class="login-form">
         <input type="text" placeholder="Username or Email" required />
         <input type="password" placeholder="Password" required />
